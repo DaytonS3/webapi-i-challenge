fbf7130db33311481025626550221032be3bbdf0
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-22 16:06:52 -0400

    /api/users GET complete

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // implement your API here
 const express = require("express");
+//local
+const db = require("./data/db");
 
 const server = express();
 
 server.get("/", (req, res) => {
   res.send("Server Running...");
+});
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.json({ error: err, message: "GET REQ FAILED" });
+    });
 });
 
 server.listen(5000, () => {
