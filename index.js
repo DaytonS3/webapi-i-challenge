54ca2255116bc5ea25baf6e227b4793f8c805607
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-22 15:51:49 -0400

    Server Running

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Server Running...");
+});
+
+server.listen(5000, () => {
+  console.log("API RUNNING");
+});
